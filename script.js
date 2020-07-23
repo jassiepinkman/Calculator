d6758a1713cae5c3f53827783ab515b806937d27
jassiepinkman <jaskaran09.ja@gmail.com>
2020-07-23 17:20:55 +0530

    calculator code

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalButton = document.querySelector('[data-equals]');
+const previousOperandAndTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandAndTextElement = document.querySelector('[data-current-operand]');
+
